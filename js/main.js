8a30ebfac39335a2b9123dbaf4960e632dfe78c0
Javi1499 <fruiz1@ucol.mx>
2021-10-02 12:25:22 -0500

    mm.

--- js/main.js
+++ js/main.js
@@ -2,16 +2,16 @@ const btn = document.querySelector("#btnSubmit");
 
 
 btn.addEventListener("click", (event) => {
-    // event.preventDefault()
+     event.preventDefault()
     const p1 = document.querySelector("#p1").value;
     const p2 = document.querySelector("#p2").value;
     const p3 = document.querySelector("#p3").value;
     console.log(p1, p2, p3)
     if (p1 == 22 && p2 == "05:00" && p3 == 8.8) {
-        window.location.href = "/final.html";
-        return;
+        return window.location.href = "/final.html";
     }
-    alert("Ups!! Te equivocaste en una respuesta, verifica que escribiste correctamente")
+  
+   return  alert("Ups!! Te equivocaste en una respuesta, verifica que escribiste correctamente")
 })
 
 
