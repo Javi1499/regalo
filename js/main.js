80d5e11c8da1f8c27327414f36900e78805c0c5e
Javi1499 <fruiz1@ucol.mx>
2021-10-02 12:05:31 -0500

    Firts commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const btn = document.querySelector("#btnSubmit");
+
+
+btn.addEventListener("click", (event) => {
+    // event.preventDefault()
+    const p1 = document.querySelector("#p1").value;
+    const p2 = document.querySelector("#p2").value;
+    const p3 = document.querySelector("#p3").value;
+    console.log(p1, p2, p3)
+    if (p1 == 22 && p2 == "05:00" && p3 == 8.8) {
+        window.location.href = "/final.html";
+    }
+    alert("Ups!! Te equivocaste en una respuesta, verifica que escribiste correctamente")
+})
+
+
+
+
